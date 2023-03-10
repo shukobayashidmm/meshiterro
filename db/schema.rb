3d0b6413223b80df753e5a2dffc194ccad22922b
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-10 03:29:41 +0000

    10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_09_084815) do
+ActiveRecord::Schema.define(version: 2023_03_10_020231) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
