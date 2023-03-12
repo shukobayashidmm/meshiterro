839c4e84874f65ca77366e2a11c04546d1942482
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-12 10:00:24 +0000

    git add .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
@@ -20,5 +23,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image)
   end
 
-
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
+  end
 end
